3124f84186660bb218d7cd145721c941fd7aae39
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2023-07-03 18:24:15 +0100

    added alumini page

--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,30 @@
-    
-        /*
+/*
         =========================================
         Script written for ECE UNN WEBSITE
         ===================================
          */
-       
-        //get open and close toggleBtns
-        const toggleBtn = document.getElementById("toggle-btn");
-        const closeToggleBtn = document.querySelector(".close-toggle-btn");
-        const navbarContainer = document.querySelector(".navbar-container");
 
+//get open and close toggleBtns
+const toggleBtn = document.getElementById("toggle-btn");
+const closeToggleBtn = document.querySelector(".close-toggle-btn");
+const navbarContainer = document.querySelector(".navbar-container");
 
-        //open navabr container when the button is toggle
-        toggleBtn.addEventListener("click",(e)=>{
-        if(navbarContainer.classList.contains('close-toggle-menu')){
-        navbarContainer.classList.remove("close-toggle-menu");
-        }
-        navbarContainer.classList.add("show-toggle-menu");
-        })
+//open navabr container when the button is toggle
+toggleBtn.addEventListener("click", (e) => {
+  if (navbarContainer.classList.contains("close-toggle-menu")) {
+    navbarContainer.classList.remove("close-toggle-menu");
+  }
+  navbarContainer.classList.add("show-toggle-menu");
+});
 
-        //remove the navbar menu on scroll
-        window.onscroll = () => {
-        if(window.scrollY > 600){
-        navbarContainer.classList.add("close-toggle-menu")
-        }
-        }
+//remove the navbar menu on scroll
+window.onscroll = () => {
+  if (window.scrollY > 600) {
+    navbarContainer.classList.add("close-toggle-menu");
+  }
+};
 
-        // close the toggle menu
-        closeToggleBtn.addEventListener("click",()=>{
-        navbarContainer.classList.add("close-toggle-menu");
-        })
-        
+// close the toggle menu
+closeToggleBtn.addEventListener("click", () => {
+  navbarContainer.classList.add("close-toggle-menu");
+});
